bbd2136437a81be5f6796a8997a0869b272d8a20
Gamal Schmuck <gamal.schmuck@outlook.com>
2018-08-15 13:36:07 +0200

    Reduce amount of Ether

--- packages/smartcontracts/scripts/ganache.js
+++ packages/smartcontracts/scripts/ganache.js
@@ -19,49 +19,49 @@ const options = {
     {
       secretKey:
         '0x9bc670d2cf4adf0a20cb44f5ff32cf2b3f5d7c3629e6f1be3600af17a1fc2fc7',
-      balance: 1000000000000000000000000,
+      balance: 500 * 10 ** 18,
       usecase: 'owner main'
     },
     {
       secretKey:
         '0x646f3ee8a04290ca5a6f41b05d3f365192803fda07df5540f957db265871609c',
-      balance: 1000000000000000000000000,
+      balance: 500 * 10 ** 18,
       usecase: 'owner test user in unit tests'
     },
     {
       secretKey:
         '0x3354bc2a5a15140770d54384e96b2d2a09c03f793395e116789a14a5e20b3a38',
-      balance: 1000000000000000000000000,
+      balance: 50 * 10 ** 18,
       usecase: 'owner unused'
     },
     {
       secretKey:
         '0x1f943318127832a56aadc8f23925501c342d29144b400eb28128cea9d2d5f61c',
-      balance: 1000000000000000000000000,
+      balance: 50 * 10 ** 18,
       usecase: 'owner unused'
     },
     {
       secretKey:
         '0x0d04c7cd724628994376ed1a8a230d5440497c92633f2ebdef02064d57ae9e25',
-      balance: 1000000000000000000000000,
+      balance: 50 * 10 ** 18,
       usecase: 'test user <pmizel@asure.io>'
     },
     {
       secretKey:
         '0x92a2d8a63b4dbd7f942e0d16da24df6b9ad2b1caac87d41ad321f262f8f77407',
-      balance: 1000000000000000000000000,
+      balance: 50 * 10 ** 18,
       usecase: 'test user <fraetz@asure.io>'
     },
     {
       secretKey:
         '0x04b0390e9baa7dde1bfc135eadb0dc8b0af15df905ac74d965a91e03207dafbf',
-      balance: 1000000000000000000000000,
+      balance: 50 * 10 ** 18,
       usecase: 'test user <gschmuck@asure.io>'
     },
     {
       secretKey:
         '0x7bd310f008415a83ede5f1e84000d95a7591b26c9a38838320f9cfbe47132548',
-      balance: 1000000000000000000000000,
+      balance: 50 * 10 ** 18,
       usecase: 'test user <mlurz@asure.io>'
     }
   ],

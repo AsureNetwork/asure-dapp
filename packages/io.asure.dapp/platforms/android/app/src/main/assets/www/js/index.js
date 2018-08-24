417c71b3f5eb3be1c23c11a116790e542886a11e
Paul Mizel <email@pmizel.de>
2018-08-24 22:46:21 +0200

    add cordova

---
+++ packages/io.asure.dapp/platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,29 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+    this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+
+        // Here, we redirect to the web site.
+        var targetUrl = "https://dapp.asure.io/";        
+        window.location.replace(targetUrl);
+	},
+    // Note: This code is taken from the Cordova CLI template.
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
